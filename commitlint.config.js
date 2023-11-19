014181938aee4b052405542576c3313cc3652f5b
asrafilll <asrafilr@gmail.com>
2023-11-19 14:52:36 +0700

    chore: fix commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    "type-enum": [2, "always", ["chore", "feat", "fix", "refactor"]],
+    'type-enum': [2, 'always', ['chore', 'feat', 'fix', 'refactor']],
   },
 };
